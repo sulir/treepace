d9430eba763c2db516ca658e4d949e71d9f7de0b
Matúš Sulír <matus.sulir@gmail.com>
2014-01-28 22:53:37 +0100

    Implement loading and saving (tree formats)

--- tests/test_formats.py
+++ tests/test_formats.py
@@ -1,37 +1,44 @@
 import unittest
 from treepace.formats import *
+from textwrap import dedent
 
 class TestFormats(unittest.TestCase):
-    INDENTED = '''root
-                      item1
-                          sub
-                              subsub
-                      item2
-                      item3'''
+    INDENTED = '''\
+                root
+                    item1
+                        sub
+                            subsub
+                    item2
+                    item3'''
     PARENTHESIZED = 'root (item1 (sub (subsub)) item2 item3)'
-    XML = '''<root>
-                 <item1>
-                     <sub>
-                         <subsub />
-                     </sub>
-                 </item1>
-                 <item2 />
-                 <item3 />
-             </root>'''
+    XML = '''\
+            <root>
+                <item1>
+                    <sub>
+                        <subsub/>
+                    </sub>
+                </item1>
+                <item2/>
+                <item3/>
+            </root>'''
     TREE = Node('root', [Node('item1', [Node('sub', [Node('subsub')])]),
                          Node('item2'),
                          Node('item3')])
     
     def test_load_indented(self):
         self.assertEqual(load_indented(self.INDENTED), self.TREE)
-        with self.assertRaises(MultipleRootsError):
+        with self.assertRaises(InvalidFormatError):
             load_indented('root1\nroot2')
     
     def test_save_indented(self):
-        self.assertEqual(save_indented(self.TREE), self.INDENTED)
+        result = save_indented(self.TREE).splitlines()
+        expected = dedent(self.INDENTED).splitlines()
+        self.assertEqual(result, expected)
     
     def test_load_par(self):
         self.assertEqual(load_par(self.PARENTHESIZED), self.TREE)
+        self.assertRaises(InvalidFormatError, lambda: load_par('a(('))
+        self.assertRaises(InvalidFormatError, lambda: load_par('a b'))
     
     def test_save_par(self):
         self.assertEqual(save_par(self.TREE), self.PARENTHESIZED)
@@ -40,4 +47,6 @@ class TestFormats(unittest.TestCase):
         self.assertEqual(load_xml(self.XML), self.TREE)
     
     def test_save_xml(self):
-        self.assertEqual(save_xml(self.TREE), self.XML)
+        result = save_xml(self.TREE).splitlines()
+        expected = dedent(self.XML).splitlines()
+        self.assertEqual(result, expected)
--- treepace/formats.py
+++ treepace/formats.py
@@ -5,31 +5,103 @@ In contrast to resource-mapping, importing/exporting loads and saves trees to
 their external representation on demand, not after every change.
 """
 
+import math
+import re
+import textwrap
+from xml.dom import minidom
+from xml.etree import ElementTree
 from treepace.trees import Node
 
 def load_indented(string):
     """Create a tree from the tab- or space-indented string."""
-    return Node(None)
+    indent_len = None
+    stack = []
+    
+    for line in filter(str.strip, textwrap.dedent(string).splitlines()):
+        value = line.lstrip()
+        spaces = len(line) - len(value)
+        if spaces and not indent_len:
+            indent_len = spaces
+        level = math.ceil(spaces / indent_len) if indent_len else 0
+        
+        if level == 0 and stack:
+            raise InvalidFormatError("Multiple root nodes")
+        if level <= len(stack):
+            stack[level:] = [Node(value)]
+        else:
+            raise InvalidFormatError("Level too large")
+        if level > 0:
+            stack[-2].add_child(stack[-1])
+    return stack[0]
 
 def save_indented(tree, indent='    '):
     """Create a space- or tab-indented string from the tree."""
-    return ''
+    def indented(node, level):
+        result = level * indent + node.value + '\n'
+        for child in node.children:
+            result += indented(child, level + 1)
+        return result
+    
+    return indented(tree, 0)
 
 def load_par(string):
     """Create a tree from the parenthesized string."""
-    return Node(None)
+    tokens = re.findall(r'\(|\)|[^\(\)\s]+', string)
+    root = Node(tokens.pop(0))
+    
+    def parse(node):
+        while tokens:
+            token = tokens.pop(0)
+            if token == '(':
+                if not node.children:
+                    raise InvalidFormatError("Unexpected '('")
+                parse(node.children[-1])
+            elif token == ')':
+                return
+            else:
+                node.add_child(Node(token))
+    
+    if tokens and tokens.pop(0) != '(':
+        raise InvalidFormatError("Multiple root nodes")
+    parse(root)
+    return root
 
 def save_par(tree):
     """Create a parenthesized string from the tree."""
-    return ''
+    result = tree.value
+    if tree.children:
+        for i, child in enumerate(tree.children):
+            result += (' (' if i == 0 else ' ') + save_par(child)
+        result += ')'
+    
+    return result
 
 def load_xml(string):
     """Create a tree from the XML string."""
-    return Node(None)
+    doc = ElementTree.fromstring(string)
+    node = Node(doc.tag)
+    
+    def load(doc, node):
+        for child in doc:
+            new_node = Node(child.tag)
+            node.add_child(new_node)
+            load(child, new_node)
+    
+    load(doc, node)
+    return node
 
 def save_xml(tree):
     """Create an XML string from the tree."""
-    return ''
+    doc = ElementTree.Element(tree.value)
+    
+    def save(node, doc):
+        for child in node.children:
+            sub = ElementTree.SubElement(doc, child.value)
+            save(child, sub)
+    
+    save(tree, doc)
+    xml_str = ElementTree.tostring(doc, encoding='unicode')
+    return minidom.parseString(xml_str).documentElement.toprettyxml('    ')
 
-class MultipleRootsError(Exception):
+class InvalidFormatError(Exception):
     pass
