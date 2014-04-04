eff23b715cab9aa7c031dd571a6dc6e8b9eeb2f4
Matúš Sulír <matus.sulir@gmail.com>
2014-04-05 00:24:09 +0200

    Add subtree support

---
+++ tests/test_trees.py
@@ -0,0 +1,20 @@
+import unittest
+from treepace.trees import Tree, Subtree
+
+class TestSubtree(unittest.TestCase):
+    def test_add_node(self):
+        tree = Tree.load('1 (2 (3 (4 (5)) 6))')
+        subtree = Subtree([tree.node(2), tree.node(3)])
+        subtree.add_node(tree.node(1))
+        self.assertEqual(subtree.to_tree(), Tree.load('1 (2 (3))'))
+        self.assertRaises(ValueError, lambda: subtree.add_node(tree.node(5)))
+        subtree.add_node(tree.node(6))
+        self.assertEqual(subtree.to_tree(), Tree.load('1 (2 (3 6))'))
+    
+    def test_leaves(self):
+        tree = Tree.load('root (connected (leaf1) leaf2)')
+        subtree = Subtree([tree.node('root'), tree.node('connected'),
+            tree.node('leaf2')])
+        connected = [tree.node('connected')]
+        self.assertEqual(subtree.connected_leaves(), connected)
+        self.assertEqual(subtree.leaves(), connected + [tree.node('leaf2')])
--- treepace/trees.py
+++ treepace/trees.py
@@ -1,3 +1,4 @@
+from itertools import chain
 from treepace.formats import ParenText
 from treepace.nodes import Node
 
@@ -28,6 +29,19 @@ class Tree:
     def save(self, fmt):
         """Export the tree to a string in a given format."""
         return (fmt() if callable(fmt) else fmt).save_tree(self.root)
+    
+    def preorder(self):
+        """Return a generator for pre-order tree traversal."""
+        yield self.root
+        for child in self.root.children:
+            for item in Tree(child).preorder():
+                yield item
+    
+    def node(self, value):
+        """Return the first node with the given value (using string
+        comparison)."""
+        compare = lambda node: str(node.value) == str(value)
+        return next(filter(compare, self.preorder()), None)
     
     def search(self, pattern, start=None):
         """Search the whole tree for a given subtree."""
@@ -66,8 +80,68 @@ class Tree:
         return self.save(ParenText)
     
     def __eq__(self, other):
-        """Compare the whole tree with an another tree."""
+        """Values of all tree nodes are compared."""
         self_subtrees = [Tree(c) for c in self.root.children]
         other_subtrees = [Tree(c) for c in other.root.children]
         return (self.root.value == other.root.value
                 and self_subtrees == other_subtrees)
+
+
+class Subtree:
+    """A connected part of a tree with one root node and one or more leaves.
+    
+    The main tree should not be changed while its subtree is in use.
+    """
+    
+    def __init__(self, nodes):
+        """Initialize the subtree with a nonempty list of nodes."""
+        self._root = nodes[0]
+        self._nodes = {self._root}
+        for node in nodes[1:]:
+            self.add_node(node)
+    
+    def add_node(self, node):
+        """Add the node to the subtree.
+        
+        It must be connected to some node currently present in the subtree.
+        """
+        if node == self._root.parent:
+            self._root = node
+        elif node.parent not in self._nodes:
+            raise ValueError("Disconnected subtree node")
+        self._nodes.add(node)
+    
+    @property
+    def root(self):
+        """Return the current root node of the subtree."""
+        return self._root
+    
+    def leaves(self):
+        """Return all leaves of the subtree."""
+        def find_leaves(root):
+            children = [x for x in root.children if x in self._nodes]
+            return chain(*map(find_leaves, children)) if children else [root]
+        
+        return list(find_leaves(self._root))
+    
+    def connected_leaves(self):
+        """Return leaves of the subtree which have children in the main tree."""
+        return [leaf for leaf in self.leaves() if leaf.children]
+    
+    def to_tree(self):
+        """Shallow-copy nodes of the subtree into a new tree."""
+        def make_tree(subtree_node):
+            tree_node = Node(subtree_node.value)
+            for child in [x for x in subtree_node.children if x in self._nodes]:
+                tree_node.add_child(make_tree(child))
+            return tree_node
+        
+        return Tree(make_tree(self._root))
+    
+    def __repr__(self):
+        """Return a text representation of the subtree (as if it was a tree)."""
+        return str(self.to_tree())
+    
+    def __eq__(self, other):
+        """Two subtrees are equal if they contain the same set of nodes."""
+        return self._nodes == other._nodes
